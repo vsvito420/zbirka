6b4f56851a047dadc6abebc282cf590ba7e08673
VitoS <ikn0@proton.me>
2023-12-24 00:13:29 +0100

    container

---
+++ container.tf
@@ -0,0 +1,81 @@
+resource "proxmox_virtual_environment_container" "ubuntu_container" {
+  description = "Managed by Terraform"
+
+  node_name = "pve"
+  vm_id     = 300
+
+  initialization {
+    hostname = "terraform-provider-proxmox-ubuntu-container"
+
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+
+    user_account {
+      keys = [
+        trimspace(tls_private_key.ubuntu_container_key.public_key_openssh)
+      ]
+      password = random_password.ubuntu_container_password.result
+    }
+  }
+
+  network_interface {
+    name = "veth0"
+  }
+
+  operating_system {
+    template_file_id = proxmox_virtual_environment_file.ubuntu_container_template.id
+    type             = "ubuntu"
+  }
+
+  mount_point {
+    # bind mount, *requires* root@pam authentication
+    volume = "/mnt/bindmounts/shared"
+    path   = "/mnt/shared"
+  }
+
+  mount_point {
+    # volume mount, a new volume will be created by PVE
+    volume = "local-lvm"
+    size   = "10G"
+    path   = "/mnt/volume"
+  }
+
+}
+
+resource "proxmox_virtual_environment_file" "ubuntu_container_template" {
+  content_type = "vztmpl"
+  datastore_id = "local"
+  node_name    = "first-node"
+
+  source_file {
+    path = "http://download.proxmox.com/images/system/ubuntu-20.04-standard_20.04-1_amd64.tar.gz"
+  }
+}
+
+resource "random_password" "ubuntu_container_password" {
+  length           = 16
+  override_special = "_%@"
+  special          = true
+}
+
+resource "tls_private_key" "ubuntu_container_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+output "ubuntu_container_password" {
+  value     = random_password.ubuntu_container_password.result
+  sensitive = true
+}
+
+output "ubuntu_container_private_key" {
+  value     = tls_private_key.ubuntu_container_key.private_key_pem
+  sensitive = true
+}
+
+output "ubuntu_container_public_key" {
+  value = tls_private_key.ubuntu_container_key.public_key_openssh
+}
