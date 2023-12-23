fbba23337e437a59f32e29db8a154d797e54cfe5
VitoS <ikn0@proton.me>
2023-12-23 23:58:52 +0100

    proxmox

--- provider.tf
+++ provider.tf
@@ -2,3 +2,10 @@ provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
 
+provider "proxmox" {
+  endpoint = "https://10.0.0.2:8006/"
+  username = "root@pam"
+  password = "the-password-set-during-installation-of-proxmox-ve"
+  insecure = true
+  tmp_dir  = "/var/tmp"
+}
--- terraform.tf
+++ terraform.tf
@@ -5,5 +5,10 @@ terraform {
       version = "~> 4.0"
     }
   }
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+      version = "0.42.0"
+    }
+  }
 }
-
