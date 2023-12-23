d01aa5337b9fd9712db97d7bacea89858764a342
VitoS <ikn0@proton.me>
2023-12-24 00:05:29 +0100

    fixed

--- terraform.tf
+++ terraform.tf
@@ -4,8 +4,6 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = "~> 4.0"
     }
-  }
-  required_providers {
     proxmox = {
       source = "bpg/proxmox"
       version = "0.42.0"
