64eb73e96c2b80cd2da79e8e479abfe7714199f1
VitoS <ikn0@proton.me>
2023-12-23 23:28:53 +0100

    yes

---
+++ var.tf
@@ -0,0 +1,11 @@
+variable "cloudflare_api_token" {
+    type = string
+}
+
+variable "zone_id" {
+  default = "9578cbae9509bf2d61f16c910b065cec"
+}
+
+variable "domain" {
+    default = "skolan.icu"
+}
