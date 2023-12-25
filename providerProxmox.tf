81f6b20d4b870ab9ece2d71022e72eb4feecacfc
VitoS <ikn0@proton.me>
2023-12-25 04:44:15 +0100

    Add Proxmox container and Ansible provider configurations

--- providerProxmox.tf
+++ providerProxmox.tf
@@ -1,5 +1,10 @@
-provider "cloudflare" {
-  api_token = var.cloudflare_api_token
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+      version = "0.42.0"
+    }
+  }
 }
 
 provider "proxmox" {
